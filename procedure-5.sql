0bf6d2a651a2a9427e1a41188d8889c0a451bee1
jkuprina123 <jpivcaikina@inbox.lv>
2023-08-10 20:38:15 +0300

    labots saturs, pievienots kods

--- procedure-5.sql
+++ procedure-5.sql
@@ -1,1 +1,17 @@
-test 1
+CREATE OR REPLACE PROCEDURE print_contact(p_customer_id IN NUMBER)
+IS
+  r_contact contacts%ROWTYPE;
+BEGIN
+  -- get contact based on customer id
+  SELECT *
+  INTO r_contact
+  FROM contacts
+  WHERE customer_id = p_customer_id;
+
+  -- print out contact's information
+  dbms_output.put_line( r_contact.first_name || ' ' ||r_contact.last_name || '<' || r_contact.email ||'>' );
+
+EXCEPTION
+   WHEN OTHERS THEN
+      dbms_output.put_line( SQLERRM );
+END;
